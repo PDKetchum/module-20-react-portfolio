bf00ce8d8c23b3f24376e30bf3f552068428c56d
PDKetchum <pxiong265@gmail.com>
2022-12-01 22:26:45 -0600

    added download link for resume

--- reactapp/src/components/pages/AboutMe.js
+++ reactapp/src/components/pages/AboutMe.js
@@ -4,26 +4,8 @@ export default function AboutMe() {
   return (
     <div>
       <section className="aboutMe">
-        <p>I'm an aspiring software engineer based in St. Paul, Minnesota.</p>
         <img className="profile" src="./images/face.png" alt="Profile" />
-      </section>
-      <section className="skills">
-        <p>I have experience in:</p>
-        <ul className="listedSkills">
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>NodeJS</li>
-          <li>RESTful API</li>
-          <li>SQL</li>
-          <li>Sequelize</li>
-          <li>Handlebars</li>
-          <li>MongoDB</li>
-          <li>ExpressJS</li>
-          <li>Jest</li>
-          <li>Git</li>
-          <li>React</li>
-        </ul>
+        <p>I'm an aspiring software engineer based in St. Paul, Minnesota.</p>
       </section>
     </div>
   );
--- reactapp/src/components/pages/Resume.js
+++ reactapp/src/components/pages/Resume.js
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function Resume() {
-  return "resume";
+  return (
+    <div>
+      <section className="skills">
+        <p>I have experience in:</p>
+        <ul className="listedSkills">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>NodeJS</li>
+          <li>RESTful API</li>
+          <li>SQL</li>
+          <li>Sequelize</li>
+          <li>Handlebars</li>
+          <li>MongoDB</li>
+          <li>ExpressJS</li>
+          <li>Jest</li>
+          <li>Git</li>
+          <li>React</li>
+        </ul>
+      </section>
+      <a href="/Resume.pdf" download>
+        Click here to download my resume
+      </a>
+    </div>
+  );
 }
